b1eff7cfc8749d9ad9cb2ea9cc7febc01a66f5f5
Alex <68348769+AlexVeremchuk@users.noreply.github.com>
2023-12-17 15:41:32 +0300

    Pyphagoras done

--- Chess/src/pettia/players/Pythagoras.java
+++ Chess/src/pettia/players/Pythagoras.java
@@ -4,11 +4,9 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import game.core.Board;
-import game.core.GameOver;
-import game.core.GameResult;
-import game.core.Move;
-import game.core.PieceColor;
+import game.core.*;
+import pettia.moves.Capture;
+import game.core.moves.ICaptureMove;
 
 /**
  * TODO Веремчук Алексей
@@ -16,10 +14,74 @@ import game.core.PieceColor;
 public class Pythagoras extends PettiaPlayer {
 	private int MAX_MOVES = 80;
 	final Comparator<? super Move> brain = (m1, m2) -> getMoveWeight(m2) - getMoveWeight(m1);
-	
+
 	private int getMoveWeight(Move m) {
-		return 0;
+
+		int weight = 0;
+
+		if (m instanceof ICaptureMove) {
+			weight += 5 * ((ICaptureMove) m).getCaptured().size() * ((ICaptureMove) m).getCaptured().size();
+		}
+
+		if (m instanceof Capture){
+			Capture capture = (Capture) m;
+			Square source = capture.getSource();
+			Square target = capture.getTarget();
+			Board board = target.getBoard();
+
+			boolean isSourceCorner = isCornerSquare(source, board);
+			boolean isTargetCorner = isCornerSquare(target, board);
+
+			if (!isSourceCorner && !isTargetCorner) {
+				int distance = calculateManhattanDistance(source, target);
+				weight += 10 - distance; // Decrease weight as distance increases
+			}
+
+			if (isSourceCorner) {
+				weight -= 5;
+			}
+
+			if (isTargetCorner) {
+				weight += 5;
+			}
+		}
+
+		Piece piece = m.getPiece();
+
+		try {
+			m.doMove();
+		} catch (GameOver e) {
+			if (isWinningMove(piece, e)) {
+				weight += 10000;
+			}
+		} finally {
+			m.undoMove();
+		}
+
+		return weight;
 	}
+
+	private boolean isCornerSquare(Square square, Board board) {
+		int h = square.h;
+		int v = square.v;
+		int nH = board.nH;
+		int nV = board.nV;
+		return (h == 0 || h == nH - 1) && (v == 0 || v == nV - 1);
+	}
+
+	private int calculateManhattanDistance(Square source, Square target) {
+		int dx = Math.abs(source.h - target.h);
+		int dy = Math.abs(source.v - target.v);
+		return dx + dy;
+	}
+
+	private boolean isWinningMove(Piece piece, GameOver gameOver) {
+		return (piece.isWhite() && gameOver.result == GameResult.WHITE_WIN) ||
+				(piece.isBlack() && gameOver.result == GameResult.BLACK_WIN);
+	}
+
+
+
 
 	@Override
 	public String getName() {
